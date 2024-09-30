b4b34eee887b0c0cf37450e51d852034ec463966
paulojosegit <paulojosegm@gmail.com>
2024-09-30 18:51:37 -0300

    ...

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 import sys
+from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout
+
 from window import MainWindow
 from display import Display
-
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout
 
 app = QApplication(sys.argv)
 window = MainWindow()
